c4a63179b98700e9d6c4774b1ecf2adf903998b2
zzstoatzz <thrast36@gmail.com>
2024-06-02 01:05:58 -0500

    pare down

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 """An assistant that proofs out Prefect usage by running commands in a Docker container."""
 
-import subprocess
 from pathlib import Path
 from typing import Self
 
 import docker
 import docker.errors
-from gh_util.functions import fetch_directory_structure
 from marvin.beta.applications import (
     Application,  # application is just an Assistant + a state dict
 )
@@ -14,29 +12,14 @@ from pydantic import BaseModel, Field, PrivateAttr, model_validator
 from raggy.vectorstores.tpuf import query_namespace
 
 
-def _run_git_command(*cmd_parts: str) -> str:
-    return subprocess.check_output(["git", *cmd_parts]).decode("utf-8").strip()
-
-
-class GitInfo(BaseModel):
-    """scoops info from the current git cli context"""
-
-    owner: str = Field(default_factory=lambda: _run_git_command("config", "user.name"))
-    repo: str = Field(
-        default_factory=lambda: _run_git_command("rev-parse", "--show-toplevel")
-    )
-    branch: str = Field(
-        default_factory=lambda: _run_git_command("rev-parse", "--abbrev-ref", "HEAD")
-    )
-
-
 class Knapsack(BaseModel):
-    """Curated state useful for performing prefect-related tasks."""
+    """Curated state useful for performing prefect-related tasks.
 
-    namespace: str = Field(
-        default="marvin-slackbot",
-        description="The namespace to search for Prefect-related concepts.",
-    )
+    Includes tool implementations for:
+    - reading/writing new Python files
+    - researching Prefect concepts
+    - running scripts/commands in a sandboxed Docker environment
+    """
 
     scratchpad: Path = Field(
         default=Path.cwd() / "scratchpad",
@@ -52,9 +35,12 @@ class Knapsack(BaseModel):
     )
 
     _docker_image_name: str = PrivateAttr(default="prefect-sandbox")
-    _git_info: GitInfo = PrivateAttr(default_factory=GitInfo)
 
-    def write_new_tool(self, filename: str, body: str) -> None:
+    _namespace: str = PrivateAttr(
+        default="marvin-slackbot",
+    )
+
+    def create_or_update_scripts(self, filename: str, body: str) -> None:
         """Write a new Python file to the scratchpad directory. May be
         used to rewrite existing files.
 
@@ -67,12 +53,10 @@ class Knapsack(BaseModel):
             ...     import sys
             ...     from datetime import datetime, timedelta
             ...
-            ...     def get_current_date():
-            ...         return datetime.now().date()
-            ...
             ...     if __name__ == "__main__":
             ...         try:
             ...             n = int(sys.argv[1])
+            ...             print(datetime.now().date() - timedelta(days=n))
             ...         except ValueError:
             ...             print("Please provide an integer as an argument.")
             ...             sys.exit(1)
@@ -84,6 +68,18 @@ class Knapsack(BaseModel):
         except Exception as e:
             return f"Failed to write new helper: {e}"
 
+    def delete_script(self, filename: str) -> None:
+        """Delete a Python file from the scratchpad directory.
+
+        Example:
+        "delete the file named 'get_current_date_n_days_ago.py'"
+        >>> delete_script("get_current_date_n_days_ago.py")
+        """
+        try:
+            (self.scratchpad / f"{filename}").unlink()
+        except Exception as e:
+            return f"Failed to delete helper: {e}"
+
     async def research_prefect(self, query: str, n_documents: int = 3) -> list[str]:
         """Research a prefect-related concept. MUST be used before writing any code.
 
@@ -92,36 +88,25 @@ class Knapsack(BaseModel):
         >>> research_prefect("how to write a prefect task")
         """
         return await query_namespace(
-            query_text=query, top_k=n_documents, namespace=self.namespace
-        )
-
-    async def see_directory_structure(self) -> str:
-        """Show a fancy `tree`-like representation of the directory structure of the repo."""
-        return await fetch_directory_structure(
-            owner=self._git_info.owner,
-            repo=self._git_info.repo,
-            branch=self._git_info.branch,
+            query_text=query, top_k=n_documents, namespace=self._namespace
         )
 
     def run_command(self, command: list[str]) -> str:
-        """Run a file from the scratchpad directory in a sandboxed environment or any other
-        command that is passed as a list of strings to be executed in the Docker container.
+        """Run any linux command (including Python scripts) in a sandboxed
+        docker container with Prefect installed. All paths are RELATIVE to repo root.
+        NEVER use absolute paths.
 
-        To run a Python file located in the scratchpad directory, pass the file path
-        RELATIVE to repo root as an argument to the Python interpreter.
+        Examples:
+            run_command(["python", "scratchpad/hello_world.py"]) # run existing py file
 
-        For example, if you have a file named `hello_world.py` in the scratchpad directory,
-        you can run it by calling:
+            run_command(["cat", "scratchpad/hello_world.py"]) # view contents of existing file
 
-            run_command(["python", "scratchpad/hello_world.py"])
+            run_command(["ls", "-l"]) # list files in current directory
 
-        Or you can read an existing file to see its contents:
-
-            run_command(["cat", "scratchpad/hello_world.py"])
+            run_command(["ls", "-l", "scratchpad"]) # list files in scratchpad directory
 
         Returns:
             str: The output of the executed command.
-
         """
         client = docker.from_env()
         try:
@@ -140,7 +125,7 @@ class Knapsack(BaseModel):
         except Exception as e:
             return f"Failed to run Python file: {e}"
 
-    def list_custom_scripts(self) -> list[str]:
+    def list_scripts(self) -> list[str]:
         """List the names of all custom Python files in the scratchpad directory."""
         return [file.name for file in self.scratchpad.rglob("*.py")]
 
@@ -153,7 +138,7 @@ class Knapsack(BaseModel):
         return self
 
     @model_validator(mode="after")
-    def ensure_docker_image_exists(self) -> Self:
+    def ensure_docker_image_ready(self) -> Self:
         """Ensure the Docker image 'prefect-sandbox' exists. If not, build it from the Dockerfile."""
         client = docker.from_env()
         try:
@@ -170,24 +155,21 @@ if __name__ == "__main__":
     my_bag = Knapsack()
     with Application(
         name="Prefect code assistant",
-        description=(
-            "Answer Prefect questions by trial and error using a sandboxed environment. "
+        instructions=(
+            "Act as a Prefect code copilot in a sandboxed environment. "
             "We are using a brand new version of Prefect so you MUST rely on `research_prefect` "
             "in order to know valid Prefect syntax and imports. Never write code without research."
             "List, write and use custom Python functions to help you along the way. "
+            "If you repeatedly fail with the same error, stop and await further instructions. "
+            "Remember to codify lessons learned in the `notes` attribute when profound."
         ),
         state=my_bag,
         tools=[
             my_bag.research_prefect,
-            my_bag.write_new_tool,
+            my_bag.create_or_update_scripts,
+            my_bag.delete_script,
             my_bag.run_command,
-            my_bag.list_custom_scripts,
-            my_bag.see_directory_structure,
+            my_bag.list_scripts,
         ],
     ) as app:
-        app.chat(
-            initial_message=(
-                "what custom tools do you have? "
-                "tell me about charizard if you have such a tool"
-            )
-        )
+        app.chat(initial_message="what custom tools are available?")
