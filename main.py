ad22a062ccfe4f94c6a3e0a135a86ee7798cc897
zzstoatzz <thrast36@gmail.com>
2024-06-01 23:22:32 -0500

    a few more things

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 """An assistant that proofs out Prefect usage by running commands in a Docker container."""
 
+import subprocess
 from pathlib import Path
 from typing import Self
 
 import docker
 import docker.errors
+from gh_util.functions import fetch_directory_structure
 from marvin.beta.applications import (
     Application,  # application is just an Assistant + a state dict
 )
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, PrivateAttr, model_validator
 from raggy.vectorstores.tpuf import query_namespace
+
+
+def _run_git_command(*cmd_parts: str) -> str:
+    return subprocess.check_output(["git", *cmd_parts]).decode("utf-8").strip()
+
+
+class GitInfo(BaseModel):
+    """scoops info from the current git cli context"""
+
+    owner: str = Field(default_factory=lambda: _run_git_command("config", "user.name"))
+    repo: str = Field(
+        default_factory=lambda: _run_git_command("rev-parse", "--show-toplevel")
+    )
+    branch: str = Field(
+        default_factory=lambda: _run_git_command("rev-parse", "--abbrev-ref", "HEAD")
+    )
 
 
 class Knapsack(BaseModel):
     """Curated state useful for performing prefect-related tasks."""
-
-    docker_image_name: str = Field(
-        default="prefect-sandbox",
-        description="The name of the Docker image used to run Python files.",
-    )
 
     namespace: str = Field(
         default="marvin-slackbot",
@@ -37,6 +50,9 @@ class Knapsack(BaseModel):
         description="A place to keep useful nuggets of information based on past errors.",
         max_length=10,
     )
+
+    _docker_image_name: str = PrivateAttr(default="prefect-sandbox")
+    _git_info: GitInfo = PrivateAttr(default_factory=GitInfo)
 
     def write_new_tool(self, filename: str, body: str) -> None:
         """Write a new Python file to the scratchpad directory. May be
@@ -79,12 +95,20 @@ class Knapsack(BaseModel):
             query_text=query, top_k=n_documents, namespace=self.namespace
         )
 
+    async def see_directory_structure(self) -> str:
+        """Show a fancy `tree`-like representation of the directory structure of the repo."""
+        return await fetch_directory_structure(
+            owner=self._git_info.owner,
+            repo=self._git_info.repo,
+            branch=self._git_info.branch,
+        )
+
     def run_command(self, command: list[str]) -> str:
         """Run a file from the scratchpad directory in a sandboxed environment or any other
         command that is passed as a list of strings to be executed in the Docker container.
 
         To run a Python file located in the scratchpad directory, pass the file path
-        RELATIVE to the scratchpad directory as an argument to the Python interpreter.
+        RELATIVE to repo root as an argument to the Python interpreter.
 
         For example, if you have a file named `hello_world.py` in the scratchpad directory,
         you can run it by calling:
@@ -102,7 +126,7 @@ class Knapsack(BaseModel):
         client = docker.from_env()
         try:
             result = client.containers.run(
-                self.docker_image_name,
+                self._docker_image_name,
                 command,
                 volumes={
                     str(self.scratchpad.absolute()): {
@@ -133,10 +157,10 @@ class Knapsack(BaseModel):
         """Ensure the Docker image 'prefect-sandbox' exists. If not, build it from the Dockerfile."""
         client = docker.from_env()
         try:
-            client.images.get(self.docker_image_name)
+            client.images.get(self._docker_image_name)
         except docker.errors.ImageNotFound:
             print(
-                f"Docker image {self.docker_image_name!r} not found. Building from Dockerfile..."
+                f"Docker image {self._docker_image_name!r} not found. Building from Dockerfile..."
             )
             client.images.build(path=".", tag="prefect-sandbox")
         return self
@@ -158,6 +182,7 @@ if __name__ == "__main__":
             my_bag.write_new_tool,
             my_bag.run_command,
             my_bag.list_custom_scripts,
+            my_bag.see_directory_structure,
         ],
     ) as app:
         app.chat(
